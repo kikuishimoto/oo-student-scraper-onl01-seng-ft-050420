b71c832e2243e149a8340f836cb753f3f02886cb
kiku ishimoto <kikuish@gmail.com>
2020-05-24 23:11:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,8 +7,15 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     scraped_students = []
-
-    doc.css('div.roster-cards-container').text
+    doc.css("div.roster-cards-container").each do |card|
+      card.css("div.student-card").each do |student|
+        student_profile_link = "#{student.attribute('href')}"
+        student_name = student.css(".student_name").text
+        student_location = student.css(".student_location").text
+        scraped_students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end 
+    end 
+    scraped_students
 
   end
 
