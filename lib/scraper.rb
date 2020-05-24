ce19c8b9ffed2a380a7e533fc5feb90804ffabc0
kiku ishimoto <kikuish@gmail.com>
2020-05-24 22:25:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
 
-    doc.css(div.roster-cards-container).text
+    doc.css('div.roster-cards-container').text
 
   end
 
