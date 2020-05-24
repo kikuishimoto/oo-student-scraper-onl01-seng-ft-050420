45a609788bf53f063a09a1115e9615c6873c56bb
kiku ishimoto <kikuish@gmail.com>
2020-05-24 23:12:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,8 +10,8 @@ class Scraper
     doc.css("div.roster-cards-container").each do |card|
       card.css("div.student-card").each do |student|
         student_profile_link = "#{student.attribute('href')}"
-        student_name = student.css(".student_name").text
-        student_location = student.css(".student_location").text
+        student_name = student.css(".student-name").text
+        student_location = student.css(".student-location").text
         scraped_students << {name: student_name, location: student_location, profile_url: student_profile_link}
       end
     end
