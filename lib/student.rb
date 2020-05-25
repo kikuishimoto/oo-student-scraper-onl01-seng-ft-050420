2a4b6e708f24822f496c915654c8d3654904513d
kiku ishimoto <kikuish@gmail.com>
2020-05-25 00:44:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,14 +12,20 @@ class Student
   end
 
   def self.create_from_collection(students_array)
+    students_array.each do |student_hash|
+      self.new(student_hash)
+    end 
 
   end
 
   def add_student_attributes(attributes_hash)
+    attributes_hash.each{ |key, value|
+    self.send(("#{key}="), value)}
 
   end
 
   def self.all
+    @@all
 
   end
 end
